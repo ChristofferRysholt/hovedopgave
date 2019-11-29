4ea2f49c3facdef6aec48479897c0d152ca24579
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-11-29 11:44:25 +0100

    footer changes

--- templates/include/footer.php
+++ templates/include/footer.php
@@ -1,23 +1,29 @@
-            <footer>
-                <section class="footer-section">
-                    <h2>Kontakt Info</h2>
-                    <p>Text</p>
-                    <p>Text</p>
-                    <p>Text</p>
-                </section>      
-                <section class="footer-section">
-                    <h2>Partnere</h2>
-                    <p>Text</p>
-                    <p>Text</p>
-                    <p>Text</p>
-                </section>
-                <section class="footer-section">
-                    <h2>SoMe</h2>
-                    <p>Text</p>
-                    <p>Text</p>
-                    <p>Text</p>
-                </section>
-            </footer>
+                <footer>
+                    <section class="footer-section">
+                        <h2>Kontakt info</h2>
+                        <p>Stuart Harvey</p>
+                        <p>Tlf. +45 3011 6931</p>
+                        <p>stuart@skotteknive.dk</p>
+                    </section>      
+                    <section class="footer-section">
+                        <h2>Partnere</h2>
+                        <p>Text</p>
+                        <p>Text</p>
+                        <p>Text</p>
+                    </section>
+                    <section class="footer-section">
+                        <h2>SoMe</h2>
+                        <section class="footer-img">
+                            <a href="#"><img src="images/facebook-app-logo.svg" title="Facebook" alt="Skotteknive - Facebook"></a>
+                        </section>
+                        <section class="footer-img">
+                            <a href="#"><img src="images/instagram-logo.svg" title="Instagram" alt="Skotteknive - Instagram"></a>
+                        </section>
+                        <section class="footer-img">
+                            <a href="#"><img src="images/linkedin-logo.svg" title="Linkedin" alt="Skotteknive - Linkedin"></a>
+                        </section>
+                    </section>
+                </footer>
           </article>
     </div>
 </article>
