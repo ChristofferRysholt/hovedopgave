cb25db1d09e7b5448957062c1b9f87c4bbe25756
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-12-09 00:45:46 +0100

    popup added

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+function PopUp(showOrHide) {
+    if (showOrHide === 'hide') {
+        document.getElementById('pop-up-banner').style.display = "none";
+    }
+    else if(localStorage.getItem("popupTrue") !== "1" && showOrHide === 'show') {
+        document.getElementById('pop-up-banner').removeAttribute('style');
+        localStorage.setItem("popupTrue", "1");
+    }
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 1000);
+}
+function hideBanner(e) {
+    if (e.target.id == 'pop-up-banner') {
+        document.getElementById('pop-up-banner').style.display = 'none';
+        localStorage.setItem("popupTrue", "1");
+    }
+}
