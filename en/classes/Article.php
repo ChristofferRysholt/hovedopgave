c24ca9e889f78084d281a161f789c0c964d979e6
ChristofferRysholt <eaacrch@students.eaaa.dk>
2019-12-11 10:59:40 +0100

    fixed æøå not working in title and summary

--- en/classes/Article.php
+++ en/classes/Article.php
@@ -48,8 +48,8 @@ class Article
   public function __construct( $data=array() ) {
     if (isset($data['id'])) $this->id = (int) $data['id'];
     if (isset($data['publicationDate'])) $this->publicationDate = (int) $data['publicationDate'];
-    if (isset($data['title'])) $this->title = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['title']);
-    if (isset($data['summary'])) $this->summary = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]/", "", $data['summary']);
+    if (isset($data['title'])) $this->title = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]\æ\ø\å\Æ\Ø\Å/", "", $data['title']);
+    if (isset($data['summary'])) $this->summary = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\:\$ a-zA-Z0-9()]\æ\ø\å\Æ\Ø\Å/", "", $data['summary']);
     if (isset($data['content'])) $this->content = $data['content'];
     if (isset($data['imageExtension'])) $this->imageExtension = preg_replace ("/[^\.\,\-\_\'\"\@\?\!\$ a-zA-Z0-9()]/", "", $data['imageExtension']);
   }
